e2879f18c0eb32e97a863fcd0131cc2942d8aa99
Bon Nicolai Mercado <bonhokage06@gmail.com>
2023-04-17 14:09:28 +0800

    added signup

--- router/root.go
+++ router/root.go
@@ -23,6 +23,7 @@ func (router *Router) New() *chi.Mux {
 	r.Get("/contact", PageHandler(controllers.Index(nil, "contact.gohtml", "partials/layout-parts.gohtml")))
 	r.Get("/contact/{id}", PageHandler(controllers.Index(ContactHandler.Create, "contact.gohtml", "partials/layout-parts.gohtml")))
 	r.Get("/faq", PageHandler(controllers.Index(FaqHandler.Create, "faq.gohtml", "partials/layout-parts.gohtml")))
+	r.Get("/signup", PageHandler(controllers.Index(nil, "signup.gohtml", "partials/layout-parts.gohtml")))
 	r.Get("/static/*", StaticHandler(controllers.IndexStatic()))
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
